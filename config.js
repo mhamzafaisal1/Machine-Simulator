e04b7611c88942738a637a8743d7e40605db02d4
Hamza Faisal <hfaisal@ChiDry.local>
2025-08-15 08:25:46 -0500

    Added item sessions to simulator

--- config.js
+++ config.js
@@ -18,6 +18,9 @@ module.exports = {
     // Operator session tracking collection
     operatorSessionCollectionName: 'operator-session',
     
+    // Item session tracking collection
+    itemSessionCollectionName: 'item-session',
+    
         // Additional collections for state data (same data, multiple collections)
     stateMachineDailyCollectionName: 'state-machine-daily',
     stateMachineWeeklyCollectionName: 'state-machine-weekly',
